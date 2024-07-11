f31f0bfbdcbf47d9b295ce9a49627a7d01e8c9a9
shunab <shisuiucihiha@gmail.com>
2024-07-11 09:57:43 +0100

    first commit

--- src/components/UpcomingGames.js
+++ src/components/UpcomingGames.js
@@ -16,10 +16,18 @@ const UpcomingGames = () => {
             odds1="1.87"
             odds2="1.87"
             />
-            <div className="upcoming-card upcoming-card-4-col">
-                <h2>Game 2</h2>
-                <p>Odds data...</p>
-            </div>
+            <GameCard
+            className= 'upcoming-card upcoming-card-4-col'
+            tournamentIcon="/icons/events/vct_amer.png"
+            tournamentName="VCT 24: AMER Stage 2"
+            matchTime="Monday, 01:00"
+            team1Logo="/icons/teams/sen.png"
+            team1Name="Sentinels"
+            team2Logo="/icons/teams/g2.png"
+            team2Name="G2 esports"
+            odds1="1.48"
+            odds2="2.5"
+            />
             <div className="upcoming-card upcoming-card-4-col">
                 <h2>Game 3</h2>
                 <p>Odds data...</p>
