41e6053953c695905750e21ca81fd3c9dd356e44
Sérgio <sergioseco98@gmail.com>
2024-10-23 22:06:17 +0200

    Draft transaction handler

---
+++ src/app/api/relayer/create-account/route.js
@@ -0,0 +1,22 @@
+
+import { NextRequest, NextResponse } from "next/server";
+import { createAccount } from '@near-relay/server'
+
+export async function POST(req, res) {
+    try {
+        const { publicKey, accountId } = await req.json();
+
+        const receipt = await createAccount(accountId, publicKey)
+
+        return NextResponse.json(receipt, {
+            status: 200,
+            headers: { "content-type": "application/json" },
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { msg: error.toString(), error },
+            { headers: { "content-type": "application/json" }, status: 500 }
+        );
+    }
+}
