159ed19331ea90785b3dacbd2023a7bc4d648585
Zeeshan Ahmad <itexpert120@outlook.com>
2025-02-23 17:24:02 +0500

    remove class from MobileMenuDropdown

--- src/components/MobileMenuDropdown.js
+++ src/components/MobileMenuDropdown.js
@@ -51,11 +51,10 @@ const MobileMenuDropdown = ({ isLoggedIn, onLogout }) => {
               width="24"
               height="24"
               fill="white"
-              class="bi bi-list"
               viewBox="0 0 16 16"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
               />
             </svg>
